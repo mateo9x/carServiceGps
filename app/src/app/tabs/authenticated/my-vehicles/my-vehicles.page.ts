c0151b58ffb5b3ffcdc606e3367aaec898f404d6
Mateusz <mtpala@interia.pl>
2024-01-25 10:29:23 +0100

    confirm dialog before set active vehicle

--- app/src/app/tabs/authenticated/my-vehicles/my-vehicles.page.ts
+++ app/src/app/tabs/authenticated/my-vehicles/my-vehicles.page.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {VehicleService} from "../../../services/vehicle.service";
 import {Vehicle} from "../../../models/vehicle.model";
 import {ToastService, ToastType} from "../../../services/common/toast.service";
+import {Dialog} from "@capacitor/dialog";
 
 @Component({
   selector: 'my-vehicles',
@@ -25,13 +26,21 @@ export class MyVehiclesPage implements OnInit {
     });
   }
 
-  setVehicleAsActive(vehicleId: string) {
-    this.vehicleService.setVehicleAsActive(vehicleId).subscribe({
-      next: () => {
-        this.getMyVehicles();
-        this.toastService.showToast('Pojazd wybrany jako aktywny', ToastType.SUCCESS);
-      },
-      error: () => this.toastService.showToast('Nie udało ustawić się pojazdu jako aktywny', ToastType.ERROR)
+  async setVehicleAsActive(vehicleId: string) {
+    const dialog = await Dialog.confirm({
+      title: 'Potwierdź',
+      message: 'Czy na pewno chcesz ustawić wybrany pojazd jako aktywny?',
+      okButtonTitle: 'Tak',
+      cancelButtonTitle: 'Nie'
     });
+    if (dialog.value) {
+      this.vehicleService.setVehicleAsActive(vehicleId).subscribe({
+        next: () => {
+          this.getMyVehicles();
+          this.toastService.showToast('Pojazd wybrany jako aktywny', ToastType.SUCCESS);
+        },
+        error: () => this.toastService.showToast('Nie udało ustawić się pojazdu jako aktywny', ToastType.ERROR)
+      });
+    }
   }
 }
